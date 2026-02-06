98fea597093cf756f49c7dfa6c11dfc384a36df1
Christian Hasenbichler <chrish@MacBook-Pro-von-Christian.local>
2026-02-06 09:15:49 +0100

    Use Prisma SQLite driver adapter (Prisma v7)

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import prismaConfig from '@/prisma.config'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
 /* eslint-disable no-var */
 declare global {
@@ -7,9 +7,14 @@ declare global {
 }
 /* eslint-enable no-var */
 
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL ?? 'file:./dev.db',
+})
+
 const prismaClient =
   global.__mmiPrismaClient ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   })
 
